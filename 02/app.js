c6aec434af8e943f77c5acc628a74d65de928fa9
hyunS00 <optifun72@gmail.com>
2023-03-10 13:52:00 +0900

    02

---
+++ 02/app.js
@@ -0,0 +1,42 @@
+// number
+var count = 5;
+console.log(count);
+// string
+var count = '5';
+
+var nick = 'bear';
+var nick = 'bear';
+
+
+var isMan = true;
+
+var log = function(str){
+    console.log(str);
+}
+
+log(nick);
+
+var image = {};
+
+image.width = 100;
+image.height = 100;
+console.log(image);
+console.log(image.width);
+
+
+var image = ['1.jpg','2.jpg'];
+console.log(image,image.length);
+
+image[0] = '3.jpg';
+image[3] = '4.jpg';
+console.log(image);
+
+// 보통 변수를 선언할떄 초기화를 진행함
+// null로 쓰는경우는 많이 없음
+var nick = '';
+var title = -1;
+var isMan = null;
+
+var a;
+console.log(a, nick);
+
